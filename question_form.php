bb68c9655230789336c5d4e7826acc0172d60740
gmichaelguy <gmichaelguy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-01 15:02:02 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/mtouch-quiz/trunk@306553 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ question_form.php
@@ -0,0 +1,203 @@
+<?php
+require('wpframe.php');
+wpframe_stop_direct_call(__FILE__);
+
+$action = 'new';
+if($_REQUEST['action'] == 'edit') $action = 'edit';
+
+$question= $wpdb->get_row($wpdb->prepare("SELECT question, explanation, point_value FROM {$wpdb->prefix}mtouchquiz_question WHERE ID=%d", $_REQUEST['question']));
+$all_answers = $wpdb->get_results($wpdb->prepare("SELECT answer, correct, hint FROM {$wpdb->prefix}mtouchquiz_answer WHERE question_id=%d ORDER BY sort_order", $_REQUEST['question']));
+
+$answer_count = 5;
+if($action == 'edit' and $answer_count < count($all_answers)) $answer_count = count($all_answers) ;
+
+?>
+
+<div class="wrap">
+  <h2><?php echo t(ucfirst($action) . " Question"); ?></h2>
+  <div id="titlediv">
+    <input type="hidden" id="title" name="ignore_me" value="This is here for a workaround for a editor bug" />
+  </div>
+  <?php
+wpframe_add_editor_js();
+?>
+<style type="text/css">
+.qtrans_title, .qtrans_title_wrap {display:none;}
+</style>
+<script type="text/javascript">
+var answer_count = <?php echo $answer_count?>;
+
+function newAnswer() {
+	answer_count++;
+	var para = document.createElement("p");
+	var Clabel = document.createElement("label");
+	Clabel.setAttribute("for", "correct_answer_" + answer_count);
+	Clabel.appendChild(document.createTextNode("<?php e("Correct"); ?>"));
+	para.appendChild(Clabel);
+	var Cinput = document.createElement("input");
+	Cinput.setAttribute("type", "checkbox");
+	Cinput.setAttribute("name", "correct_answer");
+	Cinput.className = "correct_answer";
+	Cinput.setAttribute("value", answer_count);
+	Cinput.setAttribute("id", "correct_answer_" + answer_count);
+	para.appendChild(Cinput);
+	para.appendChild(document.createTextNode("<?php e("Delimit"); ?>"));
+	var Linput = document.createElement("input");
+	Linput.setAttribute("type", "checkbox");
+	Linput.setAttribute("name", "enclose_latex");
+	Linput.className = "enclose_latex";
+	Linput.setAttribute("value", answer_count);
+	Linput.setAttribute("id", "enclose_latex_" + answer_count);
+	para.appendChild(Linput);
+	//para.appendChild(document.createTextNode("<?php e("Answer:"); ?>"));
+	var Atextarea = document.createElement("textarea");
+	Atextarea.setAttribute("name", "answer[]");
+	Atextarea.setAttribute("rows", "3");
+	Atextarea.setAttribute("cols", "50");
+	para.appendChild(Atextarea);
+	para.appendChild(document.createTextNode("<?php e("Hint:"); ?>"));
+	var Htextarea = document.createElement("textarea");
+	Htextarea.setAttribute("name", "hint[]");
+	Htextarea.setAttribute("rows", "3");
+	Htextarea.setAttribute("cols", "50");
+	para.appendChild(Htextarea);
+	//para.innerHTML("<table><tr><td>");
+	//para.appendChild(Htextarea);
+	//para.innerHTML("</td></tr></table>");
+	//var BigTable = document.createElement("table");
+	//BigTable.
+	
+	//$("extra-answers").innerHTML += code.replace(/%%NUMBER%%/g, answer_count);
+	document.getElementById("extra-answers").appendChild(para);
+}
+function init() {
+	jQuery("#post").submit(function(e) {
+		// Make sure question is suplied
+		var contents;
+		if(window.tinyMCE && document.getElementById("content").style.display=="none") { // If visual mode is activated.
+			contents = tinyMCE.get("content").getContent();
+		} else {
+			contents = document.getElementById("content").value;
+		}
+		
+		if(!contents) {
+			alert("<?php e("Please enter the question"); ?>");
+			e.preventDefault();
+			e.stopPropagation();
+			return true;
+		}
+		
+		// We must have atleast 2 answers.
+		var answer_count = 0
+		jQuery(".answer").each(function() {
+			if(this.value) answer_count++;
+		});
+		//if(answer_count < 2) {
+		//	alert("<?php //e("Please enter atleast two answers"); ?>");
+		//	e.preventDefault();
+		//	e.stopPropagation();
+		//	return true;
+		//}
+		
+		//A correct answer must be selected.
+		var correct_answer_selected = false;
+		jQuery(".correct_answer").each(function() {
+			if(this.checked) {
+				correct_answer_selected = true;
+				return true;
+			}
+		});
+		if(!correct_answer_selected) {
+			alert("<?php e("Please select a correct answer"); ?>");
+			e.preventDefault();
+			e.stopPropagation();
+		}
+	});
+}
+jQuery(document).ready(init);
+</script>
+  <form name="post" action="edit.php?page=mtouch-quiz/question.php&amp;quiz=<?php echo $_REQUEST['quiz']; ?>" method="post" id="post">
+    <div id="poststuff">
+      <div id="<?php echo user_can_richedit() ? 'postdivrich' : 'postdiv'; ?>" class="postarea">
+        <div class="postbox">
+          <h3 class="hndle">
+            <?php e('Question') ?>
+            </span></h3>
+          <div class="inside">
+            <?php the_editor(stripslashes($question->question)); ?>
+          </div>
+        </div>
+        <div class="postbox">
+          <h3 class="hndle"><span>
+            <?php e('Answers and Hints') ?>
+            </span></h3>
+          <div class="inside">
+            <?php
+for($i=1; $i<=$answer_count; $i++) { ?>
+            <p>
+            <table>
+              <tr>
+                <td><table>
+                    <tr>
+                      <td><input type="checkbox" class="correct_answer" id="correct_answer_<?php echo $i?>" <?php if($all_answers[$i-1]->correct == 1 && $action != 'new') echo 'checked="checked"';?> name="correct_answer[]" value="<?php echo $i?>" />
+                        <label for="correct_answer_<?php echo $i?>">
+                          <?php e("Correct"); ?>
+                        </label></td>
+                    </tr>
+                    <tr>
+                      <td><input type="checkbox" id="enclose_latex_<?php echo $i?>" name="enclose_latex[]" value="<?php echo $i?>" />
+                        <label for="enclose_latex_<?php echo $i?>">
+                          <?php e("Delimit"); ?>
+                        </label></td>
+                    </tr>
+                  </table></td>
+                <td><textarea name="answer[]" class="answer" rows="3" cols="50"><?php if($action == 'edit') echo stripslashes($all_answers[$i-1]->answer); ?>
+</textarea></td>
+                <td> Hint:
+                  <textarea name="hint[]" class="hint" rows="3" cols="50"><?php if($action == 'edit') echo stripslashes($all_answers[$i-1]->hint); ?>
+</textarea>
+                  </p></td>
+              </tr>
+            </table>
+            <?php } ?>
+            
+            <div id="extra-answers"></div>
+            <a href="javascript:newAnswer();">
+            <?php e("Add New Answer"); ?>
+            </a> </div>
+        </div>
+        <div class="postbox">
+          <h3 class="hndle"><span>
+            <?php e('Explanation') ?>
+            </span></h3>
+          <div class="inside">
+            <textarea name="explanation" rows="5" cols="50"><?php echo stripslashes($question->explanation)?></textarea>
+            <br />
+            <p>
+              <?php e('You can use this field to explain the correct answer. This will be shown whenever correct answers are revealed.') ?>
+            </p>
+          </div>
+        </div>
+        <div class="postbox">
+          <h3 class="hndle"><span>
+            <?php e('Point Value') ?>
+            </span></h3>
+          <div class="inside">
+            <textarea name="point_value" rows="1" cols="3"><?php if ($action == 'new') { echo '100';} else { echo stripslashes($question->point_value);} ?>
+</textarea>
+          </div>
+        </div>
+      </div>
+      <p class="submit">
+        <input type="hidden" name="quiz" value="<?php echo $_REQUEST['quiz']?>" />
+        <input type="hidden" name="question" value="<?php echo stripslashes($_REQUEST['question'])?>" />
+        <input type="hidden" id="user-id" name="user_ID" value="<?php echo (int) $user_ID ?>" />
+        <input type="hidden" name="action" value="<?php echo $action ?>" />
+        <span id="autosave"></span>
+        <input type="submit" name="submit" value="<?php e('Save') ?>" style="font-weight: bold;" />
+      </p>
+      <a href="edit.php?page=mtouch-quiz/question.php&amp;quiz=<?php echo $_REQUEST['quiz']?>">
+      <?php e("Go to Questions Page") ?>
+      </a> </div>
+  </form>
+</div>
---
+++ wpframe.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * WPFrame
+ * A simple framework to make WP Plugin development easier.
+ */
+
+$GLOBALS['wpframe_home'] = get_option('home');
+$GLOBALS['wpframe_wordpress'] = $GLOBALS['wpframe_siteurl'] = get_option('siteurl');
+if(!$GLOBALS['wpframe_wordpress']) $GLOBALS['wpframe_wordpress'] = $GLOBALS['wpframe_home'];
+$GLOBALS['wpframe_plugin_name'] = basename(dirname(__FILE__));
+$GLOBALS['wpframe_plugin_folder'] = $GLOBALS['wpframe_siteurl'] . '/wp-content/plugins/' . $GLOBALS['wpframe_plugin_name'];
+//$GLOBALS['wpframe_plugin_data'] = get_plugin_data($GLOBALS['wpframe_plugin_name'] . '.php');
+//* :DEBUG: */ $GLOBALS['wpdb']->show_errors();
+
+if(!function_exists('wpframe_add_editor_js')) { //Make sure multiple plugins can be created using WPFrame
+
+/// Adds the JS code needed for the editor. Changes often. So made it centralized
+function wpframe_add_editor_js() {
+	wp_enqueue_script( 'common' );
+	wp_enqueue_script( 'jquery-color' );
+	wp_print_scripts('editor');
+	if (function_exists('add_thickbox')) add_thickbox();
+	wp_print_scripts('media-upload');
+	if (function_exists('wp_tiny_mce')) wp_tiny_mce();
+	wp_admin_css();
+	wp_enqueue_script('utils');
+	do_action("admin_print_styles-post-php");
+	do_action('admin_print_styles');
+}
+
+/// Make sure that the user don't call this file directly - forces the use of the WP interface
+function wpframe_stop_direct_call($file) {
+	if(preg_match('#' . basename($file) . '#', $_SERVER['PHP_SELF'])) die('Don\'t call this page directly.'); // Stop direct call
+}
+
+/// Shows a message in the admin interface of Wordpress
+function wpframe_message($message, $type='updated') {
+	if($type == 'updated') $class = 'updated fade';
+	elseif($type == 'error') $class = 'updated error';
+	else $class = $type;
+	
+	print '<div id="message" class="'.$class.'"><p>' . __($message, $GLOBALS['wpframe_plugin_name']) . '</p></div>';
+}
+
+/// Globalization function - Returns the transilated string
+function t($message) {
+	return __($message, $GLOBALS['wpframe_plugin_name']);
+}
+
+/// Globalization function - prints the transilated string
+function e($message) {
+	_e($message, $GLOBALS['wpframe_plugin_name']);
+}
+
+}
